cc14c08365f9b7272816377d67e3c54f589d73d3
Franky Lau <killalau1988@hotmail.com>
2018-04-01 22:43:09 +0800

    feat(contract): sell and get article

---
+++ chainlist/contracts/ChainList.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.18;
+
+contract ChainList {
+    // state variables
+    address seller;
+    string name;
+    string desc;
+    uint256 price;
+
+    function sellArticle(string _name, string _desc, uint256 _price) public {
+        seller = msg.sender;
+        name = _name;
+        desc = _desc;
+        price = _price;
+    }
+
+    function getArticle() public view returns (
+        address _seller, 
+        string _name, 
+        string _desc, 
+        uint256 _price
+    ) {
+        return (seller, name, desc, price);
+    }
+}
