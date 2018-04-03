58ae527382e4c6fa215e08a754cf65db64033dc2
Franky Lau <killalau1988@hotmail.com>
2018-04-03 14:30:55 +0800

    feat(contract): sell article function

--- chainlist/contracts/ChainList.sol
+++ chainlist/contracts/ChainList.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.4.18;
 contract ChainList {
     // state variables
     address seller;
+    address buyer;
     string name;
     string desc;
     uint256 price;
@@ -10,6 +11,13 @@ contract ChainList {
     // events
     event LogSellArticle (
         address indexed _seller,
+        string _name,
+        uint256 _price
+    );
+
+    event LogBuyArticle (
+        address indexed _seller,
+        address indexed _buyer,
         string _name,
         uint256 _price
     );
@@ -25,10 +33,36 @@ contract ChainList {
 
     function getArticle() public view returns (
         address _seller,
+        address _buyer,
         string _name,
         string _desc,
         uint256 _price
     ) {
-        return (seller, name, desc, price);
+        return (seller, buyer, name, desc, price);
+    }
+
+    function buyArticle() payable public {
+        // special keyword: throw, assert, require, revert
+
+        // we check whether there is an article for sale
+        require(seller != 0x0);
+
+        // we check that article has not been sold yet
+        require(buyer == 0x0);
+
+        // we don't allow the seller to buy his own article
+        require(msg.sender != seller);
+
+        // we check that the value sent corresponds to the price of the article
+        require(msg.value == price);
+
+        // keep buyer's info
+        buyer = msg.sender;
+
+        // buyer pay the seller
+        seller.transfer(msg.value);
+
+        // trigger the event
+        emit LogBuyArticle(seller, buyer, name, price);
     }
 }
--- chainlist/test/ChainListHappyPath.js
+++ chainlist/test/ChainListHappyPath.js
@@ -3,6 +3,7 @@ var ChainList = artifacts.require('./ChainList.sol');
 // test suite
 contract('ChainList', (accounts) => {
     const seller = accounts[1];
+    const buyer = accounts[2];
     const name = 'article 1';
     const desc = 'Description for article 1';
     const price = 2;
@@ -12,9 +13,10 @@ contract('ChainList', (accounts) => {
         const data = await instance.getArticle();
 
         assert.equal(data[0], 0x0, 'seller must be empty');
-        assert.equal(data[1], '', 'article name must be empty');
-        assert.equal(data[2], '', 'article description must be empty');
-        assert.equal(data[3].toNumber(), 0, 'article price must be zero');
+        assert.equal(data[1], 0x0, 'buyer must be empty');
+        assert.equal(data[2], '', 'article name must be empty');
+        assert.equal(data[3], '', 'article description must be empty');
+        assert.equal(data[4].toNumber(), 0, 'article price must be zero');
     });
 
     it('should sell an article', async () => {
@@ -23,9 +25,10 @@ contract('ChainList', (accounts) => {
         const data = await instance.getArticle();
 
         assert.equal(data[0], seller, 'seller must be ' + seller);
-        assert.equal(data[1], name, 'article name must be ' + name);
-        assert.equal(data[2], desc, 'article description must be ' + desc);
-        assert.equal(data[3].toNumber(), web3.toWei(price), 'article price must be ' + web3.toWei(price));
+        assert.equal(data[1], 0x0, 'buyer must be empty');
+        assert.equal(data[2], name, 'article name must be ' + name);
+        assert.equal(data[3], desc, 'article description must be ' + desc);
+        assert.equal(data[4].toNumber(), web3.toWei(price), 'article price must be ' + web3.toWei(price));
     });
 
     it('should emit an event when a new article is sold', async () => {
@@ -37,5 +40,30 @@ contract('ChainList', (accounts) => {
         assert.equal(receipt.logs[0].args._seller, seller, 'event seller must be ' + seller);
         assert.equal(receipt.logs[0].args._name, name, 'event name must be ' + name);
         assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(price), 'event name must be ' + web3.toWei(price));
+    });
+
+    it('should buy an article', async () => {
+        const instance = await ChainList.deployed();
+        const origBuyerBalance = web3.fromWei(web3.eth.getBalance(buyer)).toNumber();
+        const origSellerBalance = web3.fromWei(web3.eth.getBalance(seller)).toNumber();
+        const receipt = await instance.buyArticle({ from: buyer, value: web3.toWei(price) });
+        const data = await instance.getArticle();
+        const newBuyerBalance = web3.fromWei(web3.eth.getBalance(buyer)).toNumber();
+        const newSellerBalance = web3.fromWei(web3.eth.getBalance(seller)).toNumber();
+
+        assert.equal(data[0], seller, 'seller must be ' + seller);
+        assert.equal(data[1], buyer, 'buyer must be ' + buyer);
+        assert.equal(data[2], name, 'article name must be ' + name);
+        assert.equal(data[3], desc, 'article description must be ' + desc);
+        assert.equal(data[4].toNumber(), web3.toWei(price), 'article price must be ' + web3.toWei(price));
+        assert.equal(receipt.logs.length, 1, 'two event should have been triggered');
+        assert.equal(receipt.logs[0].event, 'LogBuyArticle', 'event should be LogBuyArticle');
+        assert.equal(receipt.logs[0].args._seller, seller, 'event seller must be ' + seller);
+        assert.equal(receipt.logs[0].args._buyer, buyer, 'event buyer must be ' + buyer);
+        assert.equal(receipt.logs[0].args._name, name, 'event name must be ' + name);
+        assert.equal(receipt.logs[0].args._price.toNumber(), web3.toWei(price), 'event name must be ' + web3.toWei(price));
+        assert.equal(newSellerBalance, origSellerBalance + price, 'Seller balance should update correctly');
+        assert(newBuyerBalance <= origBuyerBalance - price, 'Buyer balance should update correctly');
+
     });
 });
